611c156206872f9a97cd1cea88ac65209ccef9e5
binaryeast <binaryeast@gmail.com>
2021-07-28 17:58:08 +0000

    add is_register_payment field in users

--- djangun/users/models.py
+++ djangun/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -8,9 +8,11 @@ class User(AbstractUser):
     """Default user for Djangun PaaS project."""
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+
+    is_register_payment = models.BooleanField("결제정보 등록", default=False)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
