f5474edc6f363b2af5e142c3f7be254f967b3428
binaryeast <binaryeast@gmail.com>
2021-11-09 15:41:00 +0000

    add model string

--- djangun/saloon/models.py
+++ djangun/saloon/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+User = get_user_model()
+
+
+# 게시판 - 스레드 | 갤러리 / 언어별, 프레임워크별, 구인, 프로젝트 홍보, 후기, 잡담.
+# 위키 - 노션 | django-wiki
+# 문서 - 포스트 | gitbook
+# 템플릿 - 강좌 | 사용기?
+
+
