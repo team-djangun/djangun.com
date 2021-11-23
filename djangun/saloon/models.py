06a029c8c9175b5825aa4de4d66807162bb30a39
Gipyong <gipyong7@naver.com>
2021-11-23 09:59:33 +0000

    create saloon/models.py

--- djangun/saloon/models.py
+++ djangun/saloon/models.py
@@ -11,4 +11,25 @@ User = get_user_model()
 # 문서 - 포스트 | gitbook
 # 템플릿 - 강좌 | 사용기?
 
+class Gallary(models.Model):        #갤러리 카테고리
+    gallary_name = models.CharField(max_length = 255)
+    upper_category = models.ForeignKey(self, on_delete = models.SET_NULL)    #each language, each field
+
+
+class Post(models.Model):       #게시물
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    writter = models.ForeignKey(User, on_delete = models.CASCADE)
+    star = models.IntegerField()
+
+
+class Comment(models.Model):    #댓글
+    content = models.CharField(max_length = 1000)
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    writter = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+
+class WorkPlace(models.Model):      #개인 작업공간
+    code_txt = models.TextField()
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
 
