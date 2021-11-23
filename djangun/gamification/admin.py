fe67c82795cde4574356356ac92ab866fb422b8d
binaryeast <binaryeast@gmail.com>
2021-11-23 00:01:05 +0000

    add basic admin for gamification

--- djangun/gamification/admin.py
+++ djangun/gamification/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import (
+    Guild,
+    LevelDefinition,
+    GoalCategory,
+    Trophy,
+    Achievement,
+)
 
-# Register your models here.
+
+@admin.register(Guild)
+class GuildAdmin(admin.ModelAdmin):
+    list_display = ("guild_name", "guild_anniversary")
+
+
+@admin.register(LevelDefinition)
+class LevelDefinitionAdmin(admin.ModelAdmin):
+    list_display = ("level_phase", "level_name", "level_exp")
+
+
+@admin.register(GoalCategory)
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ("category", "next_goal") 
+
+
+@admin.register(Trophy)
+class TrophyAdmin(admin.ModelAdmin):
+    list_display = ("trophy_name", "next_trophy")
+
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ("achieve_name")
