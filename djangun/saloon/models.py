1b9d815ef082ec05d98aaa0b71fa8650721d7b55
binaryeast <binaryeast@gmail.com>
2021-11-26 06:54:52 +0000

    add fields for saloon models

--- djangun/saloon/models.py
+++ djangun/saloon/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _  # noqa
 from django_extensions.db.models import TimeStampedModel
 
 User = get_user_model()
@@ -11,24 +11,26 @@ User = get_user_model()
 # 문서 - 포스트 | gitbook
 # 템플릿 - 강좌 | 사용기?
 
-class Gallary(models.Model):        #갤러리 카테고리
-    gallary_name = models.CharField(max_length = 255)
-    upper_category = models.ForeignKey(self, on_delete = models.SET_NULL)    #each language, each field
+
+class SaloonCategory(models.Model):
+    category = models.CharField(max_length=255)
+    # each language, each field
+    upper_category = models.ForeignKey("self", on_delete=models.SET_NULL)
 
 
-class Post(models.Model):       #게시물
-    title = models.CharField(max_length = 255)
+class Gallary(models.Model):  # 갤러리 카테고리
+    gallary_name = models.CharField(max_length=255)
+    gallary_supporter = models.ManyToManyField(User)  # 갤러리 봉사자 or 완장 or 관리자.
+
+
+class Post(TimeStampedModel):  # 게시물
+    title = models.CharField(max_length=1000)
     content = models.TextField()
-    writter = models.ForeignKey(User, on_delete = models.CASCADE)
+    writter = models.ForeignKey(User, on_delete=models.CASCADE)
     star = models.IntegerField()
 
 
-class Comment(models.Model):    #댓글
-    content = models.CharField(max_length = 1000)
-    post = models.ForeignKey(Post, on_delete = models.CASCADE)
-    writter = models.ForeignKey(User, on_delete = models.CASCADE)
-    
-
-class WorkPlace(models.Model):      #개인 작업공간
-    code_txt = models.TextField()
-    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+class Comment(TimeStampedModel):  # 댓글
+    content = models.CharField(max_length=1000)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    writter = models.ForeignKey(User, on_delete=models.CASCADE)
