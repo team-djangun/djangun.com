17112f7bcd53b3cfc8bf53f197892fb8ee949d3f
binaryeast <binaryeast@gmail.com>
2021-11-27 05:25:20 +0000

    add admin

--- djangun/gunshop/admin.py
+++ djangun/gunshop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Chapter, Lecture, LectureCategory
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ("lecture", "chaptername", "is_published")
+
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ("category", "lecture_name", "tutor")
+
+
+@admin.register(LectureCategory)
+class LectureCategoryAdmin(admin.ModelAdmin):
+    list_display = ("category_name", "upper_category")
+
+
+# @admin.register()
+# class Admin(admin.ModelAdmin):
+#   list_display = ("",)
