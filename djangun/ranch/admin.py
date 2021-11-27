282bc37657bbfcf97ee0f1417c4b6eb1ee20ee58
binaryeast <binaryeast@gmail.com>
2021-11-27 15:02:48 +0000

    add ranch admin

--- djangun/ranch/admin.py
+++ djangun/ranch/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, WorkSpace
+
+
+@admin.register(WorkSpace)
+class WorkSpaceAdmin(admin.ModelAdmin):
+    list_display = ("owner", "name", "is_business", "is_public")
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("methodology", "objective", "github_url")
+
+
+# @admin.register()
+# class Admin(admin.ModelAdmin):
+#   list_display = ("",)
