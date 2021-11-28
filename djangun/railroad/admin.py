64dd6c1a22ae98f353f83e590a7bdfc6d60015e1
binaryeast <binaryeast@gmail.com>
2021-11-28 06:31:59 +0000

    add railroad admin

--- djangun/railroad/admin.py
+++ djangun/railroad/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Gun
+
+
+@admin.register(Gun)
+class GunAdmin(admin.ModelAdmin):
+    list_display = ("owner", "name", "gun_type")
