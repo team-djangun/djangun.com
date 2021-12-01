e2747b07a941491c82f8967b92a332d867bcefe4
binaryeast <binaryeast@gmail.com>
2021-12-01 14:17:16 +0900

    temp of temp

--- djangun/gunshop/urls.py
+++ djangun/gunshop/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, re_path  # noqa
 app_name = "djangun.gunshop"
 
 urlpatterns = [
-    # path("", , name=""),
+    # path("", , name="gunshop_status"),
+    # path("<str:lecture>/", , name="lecture_home"),
+    # path("<str:lecture>/<int:class>.", , name="lecture_class"),
 ]
