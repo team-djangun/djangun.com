6281c817dfac4f6daffe6d0cffcc5aa3b3147077
binaryeast <binaryeast@gmail.com>
2021-12-01 09:41:32 +0000

    temp ranch

--- djangun/ranch/urls.py
+++ djangun/ranch/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, re_path  # noqa
 
+from .views import ranch_main_view, ranch_project_list_view, ranch_project_view
+
 app_name = "djangun.ranch"
 
 urlpatterns = [
-    # path("", , name="ranch_dashboard"),
-    # path("<str:username>/", , name="users_projects"),
-    # path("<str:username>/<str:projectname>/", , name="project_home"),
+    path("", ranch_main_view, name="ranch_dashboard"),
+    path("<str:username>/", ranch_project_list_view, name="users_projects"),
+    path("<str:username>/<str:projectname>/", ranch_project_view, name="project_home"),
     # path("<str:username>/<str:projectname>/lounge/", , name="project_lounge"),
     # path("<str:username>/<str:projectname>/campfire/", , name="project_campfire"),
     # 카톡처럼 채팅 가능하고, 이슈 처리 가능한 곳? 정 아니면 issues로 롤백하지 뭐.
--- djangun/ranch/views.py
+++ djangun/ranch/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render  # noqa
+from django.views.generic import DetailView, ListView, TemplateView  # noqa
 
-# Create your views here.
+from .models import Project, WorkSpace  # noqa
+
+
+class RanchMainView(TemplateView):
+    template_name = "ranch/home.html"
+
+
+ranch_main_view = RanchMainView.as_view()
+
+
+class RanchProjectListView(ListView):
+    template_name = "ranch/project_list.html"
+    context_object_name = "projects"
+
+    def get_queryset(self):
+        my_projects = Project.objects.filter(owner=self.user)
+        return my_projects
+
+
+ranch_project_list_view = RanchProjectListView.as_view()
+
+
+class RanchProjectView(DetailView):
+    template_name = "ranch/project.html"
+    context_object_name = "project"
+    model = Project
+
+
+ranch_project_view = RanchProjectView.as_view()
