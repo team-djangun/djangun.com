13dddc8bad3579ef5359e41a5b84bb1d936d2255
binaryeast <binaryeast@gmail.com>
2021-12-01 07:44:18 +0000

    add saloon base view

--- djangun/saloon/urls.py
+++ djangun/saloon/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, re_path  # noqa
 
+from .views import saloon_cate_view, saloon_gallary_view, saloon_main_view
+
 app_name = "djangun.saloon"
 
 urlpatterns = [
-    # path("", , name="saloon_main"),
-    # path("<str:saloon_category>/", , name="saloon_category"),
-    # path("<str:saloon_category>/<str:gallary>/", , name="saloon_gallary_list"),
+    path("", saloon_main_view, name="saloon_main"),
+    path("<str:saloon_category>/", saloon_cate_view, name="saloon_category"),
+    path("<id:gallary_id>/", saloon_gallary_view, name="saloon_gallary_list"),
     # path("<str:saloon_category>/<str:gallary>/<int:postnum>/", , name="saloon_gallary_detail"),
     # path("", , name="saloon_"),
 ]
--- djangun/saloon/views.py
+++ djangun/saloon/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render  # noqa
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView, TemplateView  # noqa
 
-# Create your views here.
+from .models import Comment, Gallary, Post, SaloonCategory  # noqa
+
+
+class SaloonMainView(TemplateView):
+    # 메인 화면에 리스트가 나오도록? 이걸 리스트뷰로?
+    template_name = "saloon/main.html"
+
+
+saloon_main_view = SaloonMainView.as_view()
+
+
+class SaloonCategoryView(ListView):
+    def get_queryset(self, saloon_category):
+        category = get_object_or_404(SaloonCategory, category=saloon_category)
+        return category.gallary_set.all()
+
+
+saloon_cate_view = SaloonCategoryView.as_view()
+
+
+class SaloonGallaryListView(ListView):
+    def get_queryset(self, gallary_id):
+        gallary = get_object_or_404(Gallary, pk=gallary_id)
+        return gallary.post_set.all()
+
+
+saloon_gallary_view = SaloonGallaryListView.as_view()
