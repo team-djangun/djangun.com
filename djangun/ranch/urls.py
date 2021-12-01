8479ad95f8548fae9d5d0aed250b104e7e8b4b20
binaryeast <binaryeast@gmail.com>
2021-12-01 00:41:23 +0000

    add temp urls

--- djangun/ranch/urls.py
+++ djangun/ranch/urls.py
@@ -3,5 +3,12 @@ from django.urls import path, re_path  # noqa
 app_name = "djangun.ranch"
 
 urlpatterns = [
+    # path("", , name="ranch_dashboard"),
+    # path("<str:username>/", , name="users_projects"),
+    # path("<str:username>/<str:projectname>/", , name="project_home"),
+    # path("<str:username>/<str:projectname>/lounge", , name="project_lounge"),
+    # path("<str:username>/<str:projectname>/", , name=""),
+    # path("<str:username>/<str:projectname>/", , name=""),
+    # path("", , name=""),
     # path("", , name=""),
 ]
